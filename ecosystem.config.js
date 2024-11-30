9f34c6f19e124d7e71b5845d00f40398756a32b3
STASYA00 <0.0stasya@gmail.com>
2024-11-30 16:50:49 +0100

    fix typos in links

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: 'citylayers',
+      script: 'dist/server/src/app.js',
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3001,
+        DATABASE_URL: 'mongodb://localhost:27017/mydatabase'
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3001,
+        DATABASE_URL: 'mongodb://localhost:27017/mydatabase',
+        DOMAIN: 'localhost',
+        NEO4J_USER: 'neo4j',
+        NEO4J_PWD: '6ty6zc9rtk5ETSjQ3TUROHNv1UzNvsBHXgW8kd7WuRw',
+        NEO4J_URI: 'neo4j+s://64a663c2.databases.neo4j.io'
+      }
+    }
+  ]
+};
